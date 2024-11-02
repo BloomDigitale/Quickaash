ecbc8d551997125b3d638eedee82102efd74a44e
Yole <igbavagodswill4@gmail.com>
2024-11-02 17:15:41 +0000

    created the function to prevent form navigation if inputs are empty

--- public/script.js
+++ public/script.js
@@ -88,6 +88,7 @@ const contactInput = document.querySelector('.phone_number').value;
 const bvnInput = document.querySelector('.bvn').value;
 const dobInput = document.querySelector('.dob').value;
 const personAddInput = document.querySelector('.personal_address').value;
+const maritalStatsInput = document.querySelector('.marital_status').value;
 const nokInput = document.querySelector('.next_of_kin').value;
 const nokNumInput = document.querySelector('.nok_number').value;
 
@@ -98,27 +99,32 @@ const amountInput = document.querySelector('.loan_amount').value;
 const durationInput = document.querySelector('.loan_duration').value;
 const purposeInput = document.querySelector('.purpose').value;
 
-
 const guarantorInput = document.querySelector('.guarantor').value;
 const guarantorNumInput = document.querySelector('.guarantor_number').value;
 const occupationInput = document.querySelector('.occupation').value;
+
+const loanDetails = [];
 
 const businessNameInput = document.querySelector('.business_name').value;
 const businessAddInput = document.querySelector('.business_address').value;
 const businessGuarantorInput = document.querySelector('.business_guarantor').value;
 const businessNumInput = document.querySelector('.business_contact').value;
 
+const businessDetails = [];
+
 const bankNameInput = document.querySelector('.bank').value;
 const accNameInput = document.querySelector('.account_name').value;
 const accNumInput = document.querySelector('.account_num').value;
 const verifyAccInput = document.querySelector('.verify_account').value;
 
+const accDetails = [];
+
 const phoneManInput = document.querySelector('.phone_manufacturer').value;
 const phoneImeiInput = document.querySelector('.phone_imei').value;
 const phoneModelInput = document.querySelector('.phone_model').value;
 const phoneConInput = document.querySelector('.phone_condition').value;
-const maritalStatsInput = document.querySelector('.marital_status').value;
 
+const collateralDetails = [];
 
 
 // Image upload variables
@@ -151,12 +157,87 @@ body.addEventListener('click', (e) => {
     }
 })
 
+// each form section input tags variables
+const personalInfoInput = document.querySelectorAll('#personal_input');
+const loanDetailsInput = document.querySelectorAll('#loan_input');
+const collateralDetailsInput = document.querySelectorAll('#collateral_input');
+const businessDetailsInput = document.querySelectorAll('#business_input');
+const accDetailsInput = document.querySelectorAll('#acc_input');
+
+// checking for input changes function
+const personalInfoInputChange = (e) => {
+    if(e.target.value !== "") {
+        personalInfo.push(`${e.target.name} : ${e.target.value}`);
+        console.log(personalInfo.length);
+
+    } else{
+        console.log(`Input empty: ${e.target.name}`);
+    }
+};
+
+const loanInputChange = (e) => {
+    if(e.target.value !== "") {
+        loanDetails.push(`${e.target.name} : ${e.target.value}`);
+        console.log(loanDetails.length);  
+    } else{
+        console.log(`Input empty: ${e.target.name}`);
+    }
+};
+
+const collateralInputChange = (e) => {
+    if(e.target.value !== "") {
+        collateralDetails.push(`${e.target.name} : ${e.target.value}`);
+        console.log(collateralDetails.length);  
+    } else{
+        console.log(`Input empty: ${e.target.name}`);
+    }
+};
+
+const businessInputChange = (e) => {
+    if(e.target.value !== "") {
+        businessDetails.push(`${e.target.name} : ${e.target.value}`);
+        console.log(businessDetails.length); 
+    } else{
+        console.log(`Input empty: ${e.target.name}`);
+    }
+};
+
+const accInputChange = (e) => {
+    if(e.target.value !== "") {      
+        accDetails.push(`${e.target.name} : ${e.target.value}`);
+        console.log(accDetails,length);
+    } else{
+        console.log(`Input empty: ${e.target.name}`);
+    }
+};
+
+
+
+personalInfoInput.forEach(input => {
+    input.addEventListener('blur', personalInfoInputChange);
+});
+
+loanDetailsInput.forEach(input => {
+    input.addEventListener('blur', loanInputChange);
+});
+
+collateralDetailsInput.forEach(input => {
+    input.addEventListener('blur', collateralInputChange);
+});
+
+businessDetailsInput.forEach(input => {
+    input.addEventListener('blur', businessInputChange);
+});
+
+accDetailsInput.forEach(input => {
+    input.addEventListener('blur', accInputChange);
+});
+
+
 next_page2.addEventListener('click', (e) => {
     e.preventDefault();
 
-    
-
-    if(personalInfo.length > 10) {
+    if(personalInfo.length > 9) {
         page1.classList.remove('flex');
         page1.classList.add('hidden');
         nav1.classList.remove('flex');
@@ -180,57 +261,66 @@ next_page2.addEventListener('click', (e) => {
 next_page3.addEventListener('click', (e) => {
     e.preventDefault();
 
-    page2.classList.remove('flex');
-    page2.classList.add('hidden');
-    nav2.classList.remove('flex');
-    nav2.classList.add('hidden');
-    hero2.classList.remove('md:flex');
-    hero2.classList.add('md:hidden');
-    
-    
-    page3.classList.remove('hidden');
-    page3.classList.add('flex');
-    nav3.classList.remove('hidden');
-    nav3.classList.add('flex');
-    hero3.classList.remove('md:hidden');
-    hero3.classList.add('md:flex');
+    if(loanDetails.length === 8 ) {
+        page2.classList.remove('flex');
+        page2.classList.add('hidden');
+        nav2.classList.remove('flex');
+        nav2.classList.add('hidden');
+        hero2.classList.remove('md:flex');
+        hero2.classList.add('md:hidden');
+        
+        
+        page3.classList.remove('hidden');
+        page3.classList.add('flex');
+        nav3.classList.remove('hidden');
+        nav3.classList.add('flex');
+        hero3.classList.remove('md:hidden');
+        hero3.classList.add('md:flex');
+    }
+
 });
 
 next_page4.addEventListener('click', (e) => {
     e.preventDefault();
 
-    page3.classList.remove('flex');
-    page3.classList.add('hidden');
-    nav3.classList.remove('flex');
-    nav3.classList.add('hidden');
-    hero3.classList.remove('md:flex');
-    hero3.classList.add('md:hidden');
-    
-    
-    page4.classList.remove('hidden');
-    page4.classList.add('flex');
-    nav4.classList.remove('hidden');
-    nav4.classList.add('flex');
-    hero4.classList.remove('md:hidden');
-    hero4.classList.add('md:flex');
+    if(collateralDetails.length > 3) {
+        page3.classList.remove('flex');
+        page3.classList.add('hidden');
+        nav3.classList.remove('flex');
+        nav3.classList.add('hidden');
+        hero3.classList.remove('md:flex');
+        hero3.classList.add('md:hidden');
+        
+        
+        page4.classList.remove('hidden');
+        page4.classList.add('flex');
+        nav4.classList.remove('hidden');
+        nav4.classList.add('flex');
+        hero4.classList.remove('md:hidden');
+        hero4.classList.add('md:flex');
+    }
+
+
 });
 
 next_page5.addEventListener('click', (e) => {
     e.preventDefault();
 
-    page4.classList.remove('flex');
-    page4.classList.add('hidden');
-    nav4.classList.remove('flex');
-    nav4.classList.add('hidden');
-    hero4.classList.remove('md:flex');
-    hero4.classList.add('md:hidden');
-    
-    page5.classList.remove('hidden');
-    page5.classList.add('flex');
-    nav5.classList.remove('hidden');
-    nav5.classList.add('flex');
-    hero5.classList.remove('md:hidden');
-    hero5.classList.add('md:flex');
+    if(businessDetails.length === 5) {
+        page4.classList.remove('flex');
+        page4.classList.add('hidden');
+        nav4.classList.remove('flex');
+        nav4.classList.add('hidden');
+        hero4.classList.remove('md:flex');
+        hero4.classList.add('md:hidden');
+        
+        page5.classList.remove('hidden');
+        page5.classList.add('flex');
+        nav5.classList.remove('hidden');
+        nav5.classList.add('flex');
+        hero5.classList.remove('md:hidden');
+        hero5.classList.add('md:flex');
+    }
 });
 
 
